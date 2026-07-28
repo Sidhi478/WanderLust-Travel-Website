d6d9f679e40f621eec0071489a1496deba52715a
sidhika dangwal <dangwalsidhika@gmail.com>
2026-07-28 11:53:18 +0530

    Fix WrapAsync import in review route

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const router=express.Router({mergeParams:true});
-const WrapAsync=require("../utils/WrapAsync.js");
+const WrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js"); 
 const Review=require("../models/reviews.js");
